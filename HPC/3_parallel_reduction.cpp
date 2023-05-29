8b7772a5396f1818dc2372426f4bbfc961e4207b
Aarti Rade <aartirade@gmail.com>
2023-05-29 23:18:01 +0530

    Update 3_parallel_reduction.cpp

--- HPC/3_parallel_reduction.cpp
+++ HPC/3_parallel_reduction.cpp
@@ -43,3 +43,81 @@ int main(){
   cout << "The average is: " << average(arr, n) << '\n';
   return 0;
 }
+
+
+//using openmp
+
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+// Function for parallel reduction to find the minimum value
+int parallelMin(const std::vector<int>& data) {
+    int minVal = data[0];
+
+    #pragma omp parallel for reduction(min: minVal)
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i] < minVal) {
+            minVal = data[i];
+        }
+    }
+
+    return minVal;
+}
+
+// Function for parallel reduction to find the maximum value
+int parallelMax(const std::vector<int>& data) {
+    int maxVal = data[0];
+
+    #pragma omp parallel for reduction(max: maxVal)
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i] > maxVal) {
+            maxVal = data[i];
+        }
+    }
+
+    return maxVal;
+}
+
+// Function for parallel reduction to calculate the sum of elements
+int parallelSum(const std::vector<int>& data) {
+    int sum = 0;
+
+    #pragma omp parallel for reduction(+: sum)
+    for (size_t i = 0; i < data.size(); ++i) {
+        sum += data[i];
+    }
+
+    return sum;
+}
+
+// Function for parallel reduction to calculate the average of elements
+double parallelAverage(const std::vector<int>& data) {
+    int sum = parallelSum(data);
+    double average = static_cast<double>(sum) / data.size();
+
+    return average;
+}
+
+int main() {
+    std::vector<int> data = {5, 8, 2, 10, 3, 6, 1, 9, 4, 7};
+
+    // Parallel Minimum
+    int minVal = parallelMin(data);
+    std::cout << "Minimum Value: " << minVal << std::endl;
+
+    // Parallel Maximum
+    int maxVal = parallelMax(data);
+    std::cout << "Maximum Value: " << maxVal << std::endl;
+
+    // Parallel Sum
+    int sum = parallelSum(data);
+    std::cout << "Sum: " << sum << std::endl;
+
+    // Parallel Average
+    double average = parallelAverage(data);
+    std::cout << "Average: " << average << std::endl;
+
+    return 0;
+}
+
